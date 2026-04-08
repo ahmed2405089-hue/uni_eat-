e18d035019198a77fe867ae9b4adb9634fc19181
malak2402550L1 <malak2402550@miuegypt.edu.eg>
2026-04-08 16:04:18 +0200

    Create admin-dashboard.js

---
+++ script/admin-dashboard.js
@@ -0,0 +1,5 @@
+function loadDashboard() {
+    document.getElementById('resCount').innerText = 12;
+    document.getElementById('userCount').innerText = 50;
+    document.getElementById('orderCount').innerText = 120;
+}
