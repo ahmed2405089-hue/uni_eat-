c35349ae087f61f9ad9aeb2d7f1dae26252a8bb9
farahyousri <farah2404366@miuegypt.edu.eg>
2026-04-20 02:58:39 +0200

    .

--- script/admin.js
+++ script/admin.js
@@ -6,7 +6,7 @@ function get(key) {
 
 function loadDashboard() {
     // Assuming restaurants are defined in window.restaurants from all student.js
-    const resCount = window.restaurants ? window.restaurants.length : 0;
+    const resCount = 5;
     const userCount = 50; // Hardcoded for now, as users are not stored
     const orderCount = get("orders").length;
 
